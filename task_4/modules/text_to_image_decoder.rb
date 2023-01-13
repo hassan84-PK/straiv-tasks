91683bd85d504bec6add9b0715cbf6e718fbc259
Dev <dev@Devs-MacBook-Pro-10.local>
2023-01-13 17:28:54 +0500

    Refactored Task4

--- task_4/modules/text_to_image_decoder.rb
+++ task_4/modules/text_to_image_decoder.rb
@@ -1,28 +1,31 @@
 require 'base64'
+require 'fileutils'
 
 module TextToImageDecoder
 
   def execute(text_file_path)
-    decoded_text = read_data_from_file(text_file_path)
-    convert_decoded_text_to_image(decoded_text)
+    convert_decoded_text_to_image(*read_data_from_file(text_file_path))
   end
 
   private
-    def convert_decoded_text_to_image(decoded_text)
-      image_path = 'root/tmp/image.png'
-      # This is made in ruby script so i am using root/tmp here in case of Rails
-      # We can just use Rails.root.join('tmp', 'image.png')
-      File.open(image_path, 'wb') do |f|
-        f.write decoded_text
-      end
-      puts "Image is created with at path #{image_path}"
+    def convert_decoded_text_to_image(file_type, encoded_text)
+      path = 'root/tmp/' # This is made in ruby script so i am using root/tmp here in case of Rails. We can just use Rails.root.join('tmp', 'image.png')
+      FileUtils.mkdir_p(path) unless File.directory?(path)
+      
+      File.open(path + 'straiv.' + file_type, 'wb').write(Base64.decode64(encoded_text))
+
+      puts "Image is created with at path #{path}straiv.#{file_type}"
+    end
+
+    def retreive_file_type_from_string(string)
+      string.split('/').last.split(';').first
     end
 
     def read_data_from_file(text_file_path)
       begin
-        file = File.open(text_file_path,'r')
-        decoded_text = Base64.decode64(file.read.gsub!('data:image/png;base64,',''))
-        file.close
+        encoded_text  = File.open(text_file_path,'r').read()
+        encoded_text = encoded_text.split(',')
+        [retreive_file_type_from_string(encoded_text.first), encoded_text.last]
       rescue Errno::ENOENT
         puts 'File not found'
         return
