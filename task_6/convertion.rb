f52fcb11ffbfe64def47019226ed02dc8d1a01e7
Dev <dev@Devs-MacBook-Pro-10.local>
2023-01-14 02:36:59 +0500

    Refactored Task6

--- task_6/convertion.rb
+++ task_6/convertion.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
+
 require 'open3'
 
 input_file = 'Lorem_ipsum.pdf'
 output_file = 'output.pdf'
-#using ghost script command here, while keeoing in mind that machine have it installed already
+# using ghost script command here, while keeoing in mind that machine have it installed already
 
-command = "gs -sDEVICE=pdfwrite -dPDFACompatibilityPolicy=1 -dPDFSETTINGS"\
+command = 'gs -sDEVICE=pdfwrite -dPDFACompatibilityPolicy=1 -dPDFSETTINGS'\
           "=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=#{output_file} #{input_file}"
 
-Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+Open3.popen3(command) do |_stdin, _stdout, stderr, wait_thr|
   exit_status = wait_thr.value
-  unless exit_status.success?
-    raise "Command #{command} failed with error: #{stderr.read}"
-  else
-    puts "#{input_file} is converted to #{output_file}"
-  end
+  raise "Command #{command} failed with error: #{stderr.read}" unless exit_status.success?
+
+  puts "#{input_file} is converted to #{output_file}"
 end
