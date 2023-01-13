fad3a9d349ff4b580c53a438317674159e200c6d
Dev <dev@Devs-MacBook-Pro-10.local>
2023-01-14 02:53:28 +0500

    Refactored task 4

--- task_4/base64_to_image.rb
+++ task_4/base64_to_image.rb
@@ -1,5 +1,6 @@
-require './modules/text_to_image_decoder'
+# frozen_string_literal: true
 
+require './modules/text_to_image_decoder'
 include TextToImageDecoder
 
 text_file_path = 'base64.txt'
--- task_4/modules/text_to_image_decoder.rb
+++ task_4/modules/text_to_image_decoder.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 require 'base64'
 require 'fileutils'
 
 module TextToImageDecoder
-
   def execute(text_file_path)
-    convert_decoded_text_to_image(*read_data_from_file(text_file_path))
+    file_type, encoded_text = read_data_from_file(text_file_path)
+    return unless file_type && encoded_text
+
+    convert_decoded_text_to_image(file_type, encoded_text)
   end
 
   private
-    def convert_decoded_text_to_image(file_type, encoded_text)
-      path = 'root/tmp/' # This is made in ruby script so i am using root/tmp here in case of Rails. We can just use Rails.root.join('tmp', 'image.png')
-      FileUtils.mkdir_p(path) unless File.directory?(path)
-      
-      File.open(path + 'straiv.' + file_type, 'wb').write(Base64.decode64(encoded_text))
 
-      puts "Image is created with at path #{path}straiv.#{file_type}"
-    end
+  def convert_decoded_text_to_image(file_type, encoded_text)
+    # This is made in ruby script so here i am using root/tmp.
+    # But in case of Rails we can just use Rails.root.join('tmp', 'straiv')
+    path = 'root/tmp/'
+    FileUtils.mkdir_p(path) unless File.directory?(path)
 
-    def retreive_file_type_from_string(string)
-      string.split('/').last.split(';').first
-    end
+    File.open("#{path}straiv.#{file_type}", 'wb').write(Base64.decode64(encoded_text))
 
-    def read_data_from_file(text_file_path)
-      begin
-        encoded_text  = File.open(text_file_path,'r').read()
-        encoded_text = encoded_text.split(',')
-        [retreive_file_type_from_string(encoded_text.first), encoded_text.last]
-      rescue Errno::ENOENT
-        puts 'File not found'
-        return
-      end
-    end
+    puts "Image is created with at path #{path}straiv.#{file_type}"
+  end
+
+  def read_data_from_file(text_file_path)
+    encoded_text = File.open(text_file_path, 'r').read
+    file_type = encoded_text[%r{/(.+);}, 1]
+    encoded_text = encoded_text.split(',')[1]
+    [file_type, encoded_text]
+  rescue Errno::ENOENT
+    puts 'File not found'
+    [nil, nil]
+  end
 end
