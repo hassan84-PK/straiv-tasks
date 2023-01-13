6918b35eb4e0b2f1509dcdcc036bc627d272c90e
Dev <dev@Devs-MacBook-Pro-10.local>
2023-01-14 03:12:14 +0500

    Refactored task 2

--- task_2/count_array_and_sort.rb
+++ task_2/count_array_and_sort.rb
@@ -1,13 +1,19 @@
+# frozen_string_literal: true
+
 def print_result(result)
-	result.each_pair { |k, v| puts "#{k} => #{v}"}
+  result.each { |value| puts "#{value.first} => #{value.last}" }
 end
 
+def sort_and_reverse_result(result)
+  result.sort_by { |_, v| v }.reverse
+end
 
-values = ['bed', 'pillow', 'bed', 'pillow', 'table', 'bed',
-			 'pillow', 'door', 'light', 'minibar', 'light', 
-			 'chair', 'chair', 'light', 'light', 'hairdryer', 'soap']
+values = %w[bed pillow bed pillow table bed
+            pillow door light minibar light
+            chair chair light light hairdryer soap]
 
-print_result(values.tally.sort_by { |_,v| v }.reverse.to_h) 
+print_result(sort_and_reverse_result(values.tally))
 
-#other method which can be used is:
-print_result(values.tally.sort_by(&:last).reverse.to_h)
+# other approch which can be used is:
+puts '=' * 30
+print_result(values.tally.sort_by(&:last).reverse)
